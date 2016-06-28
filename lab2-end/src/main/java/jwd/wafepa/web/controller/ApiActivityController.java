bc332772fb233dde11ca9bfe98e4db00d68c427b
Zeljko Vukovic <zeljko.vukovic.ns@gmail.com>
2016-06-28 21:14:03 +0200

    lab2-end

---
+++ lab2-end/src/main/java/jwd/wafepa/web/controller/ApiActivityController.java
@@ -0,0 +1,99 @@
+package jwd.wafepa.web.controller;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import jwd.wafepa.model.Activity;
+import jwd.wafepa.service.ActivityService;
+
+@RestController
+@RequestMapping(value="/api/activities")
+public class ApiActivityController {
+	
+	@Autowired
+	private ActivityService activityService;
+	
+	@PostConstruct
+	public void init(){
+		activityService.save(new Activity("Swimming"));
+		activityService.save(new Activity("Running"));
+	}
+
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<Activity>> getActivities(){
+	
+		List<Activity> activities = activityService.findAll();
+		
+		return new ResponseEntity<>(activities, HttpStatus.OK);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET,
+					value="/{id}")
+	public ResponseEntity<Activity> getActivity(
+			@PathVariable Long id){
+		
+		Activity activity = activityService.findOne(id);
+		
+		if(activity == null){
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<>(activity, HttpStatus.OK);
+	}
+	
+	
+	@RequestMapping(method=RequestMethod.DELETE,
+					value="/{id}")
+	public ResponseEntity<Activity> delete(
+			@PathVariable Long id){
+		
+		Activity deleted = activityService.delete(id);
+		
+		if(deleted == null){
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<>(deleted,HttpStatus.OK);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,
+					consumes="application/json")
+	public ResponseEntity<Activity> addActivity(
+			@RequestBody Activity activity){
+		
+		if(activity.getId() !=null){
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		
+		Activity persisted = activityService.save(activity);
+		
+		return new ResponseEntity<>(persisted,HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT,
+					value="/{id}",
+					consumes="application/json")
+	public ResponseEntity<Activity> editActivity(
+			@PathVariable Long id,
+			@RequestBody Activity activity){
+		
+		if(!id.equals(activity.getId())){
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		
+		Activity persisted = activityService.save(activity);
+		
+		return new ResponseEntity<>(persisted, HttpStatus.OK);
+	}
+	
+}
